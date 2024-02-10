550318f09dd6789100823278037168c09d025e7e
Pruthvirajsinh Solanki <72735146+Pruthviraj7@users.noreply.github.com>
2024-02-09 22:27:56 -0500

    Update ApplicationDataController.cs

--- PassionProject/Controllers/ApplicationDataController.cs
+++ PassionProject/Controllers/ApplicationDataController.cs
@@ -5,7 +5,23 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+/*<summary>
 
+ * ApplicationDataController handles API requests related to pet applications and form data.
+ * It provides endpoints to retrieve application details for a specific pet.
+ *
+ * Example:
+ * To get the application form data for a pet with PetId = 1, make a GET request to:
+ *     /api/AppData/ApplicationForm?petId=1
+ *
+ * Response:
+ * If the pet exists, it returns a PetDto object with basic pet information and a list of AppDto
+ * containing details of applications associated with the specified pet.
+ * If the pet does not exist, it returns a 404 Not Found status.
+ 
+
+
+</ summary>*/
 namespace PassionProject.Controllers
 {
     public class ApplicationDataController : ApiController
