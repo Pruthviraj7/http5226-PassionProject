53c5ebe81bd7d077fa1933768e5864cc836d0bde
Pruthvirajsinh Solanki <72735146+Pruthviraj7@users.noreply.github.com>
2024-02-09 22:34:02 -0500

    Update UserDataController.cs

--- PassionProject/Controllers/UserDataController.cs
+++ PassionProject/Controllers/UserDataController.cs
@@ -3,7 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+/*<summary>
+ * UserDataController handles API requests related to user data, including listing users,
+ * creating new user entries, updating user information, and deleting user entries.
+ *
+ * Endpoints:
+ * 1. Users: GET /api/UserData/users
+ *    - Retrieves a list of UserDto containing basic information about all users.
+ *
+ * 2. CreateUser: POST /api/UserData/create
+ *    - Creates a new user entry based on the provided UserDto object in the request body.
+ *
+ * 3. DeleteUser: DELETE /api/UserData/delete/{id}
+ *    - Deletes the user entry with the specified UserId.
+ *
+ * 4. GetUser: GET /api/UserData/user/{id}
+ *    - Retrieves detailed information about the user with the specified UserId.
+ *
+ * 5. UpdateUser: PUT /api/UserData/update/{id}
+ *    - Updates the information of the user with the specified UserId based on the
+ *      provided UserDto object in the request body.
+ *
+ * Examples:
+ * 1. To list all users, make a GET request to:
+ *    /api/UserData/users
+ *
+ * 2. To create a new user entry, make a POST request to:
+ *    /api/UserData/create with the User details in the request body.
+ *
+ * 3. To delete a user entry with UserId = "exampleId", make a DELETE request to:
+ *    /api/UserData/delete/exampleId
+ *
+ * 4. To retrieve detailed information about a user with UserId = "exampleId", make a GET request to:
+ *    /api/UserData/user/exampleId
+ *
+ * 5. To update information of a user with UserId = "exampleId", make a PUT request to:
+ *    /api/UserData/update/exampleId with the updated User details in the request body.
+ *
+ * Responses:
+ * - For successful operations, it returns an Ok status.
+ * - If the specified user is not found (for deletion, retrieval, or update), it returns a
+ *   404 Not Found status.
+ * - If there are validation errors during user creation or update, it returns a BadRequest
+ *   status along with model validation errors.
 
+
+</summary>*/
 namespace PassionProject.Controllers
 {
     public class UserDataController : ApiController
