26e847c9575c1d2f5ac309d6dce14a3f612e818a
Pruthvirajsinh Solanki <72735146+Pruthviraj7@users.noreply.github.com>
2024-02-24 17:08:07 -0500

    Update ApplicationController.cs

--- PassionProject/Controllers/ApplicationController.cs
+++ PassionProject/Controllers/ApplicationController.cs
@@ -1,4 +1,5 @@
-﻿using PassionProject.Models;
+using PassionProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Web.Mvc;
@@ -9,28 +10,115 @@ namespace PassionProject.Controllers
     {
         private readonly string apiBaseUrl = "https://localhost:44377/api/appdata/";
 
-        [HttpGet]
-        [Route("Application/ApplicationList")]
-        public ActionResult ApplicationList(int petId)
+        // GET: List of Applications
+        public ActionResult AppList()
         {
             using (HttpClient client = new HttpClient())
             {
-                
-                string applicationsUrl = apiBaseUrl + $"applications?petId={petId}";
+                string applicationsUrl = apiBaseUrl + "applications";
                 HttpResponseMessage applicationsResponse = client.GetAsync(applicationsUrl).Result;
 
                 if (applicationsResponse.IsSuccessStatusCode)
                 {
                     var applications = applicationsResponse.Content.ReadAsAsync<List<AppDto>>().Result;
-
-                    
-                    ViewBag.PetId = petId;
-                    return View("~/Views/Application/AppList.cshtml", applications);
+                    return View("AppList", applications);
                 }
                 else
                 {
-                   
                     ViewBag.ErrorMessage = "Error retrieving applications.";
+                    return View("Error");
+                }
+            }
+        }
+
+        // GET: Apply for Adoption
+        public ActionResult Apply(int petId)
+        {
+            // Create a new ApplicationDto and set the PetId
+            var application = new AppDto { PetId = petId };
+
+            return View("Apply", application);
+        }
+
+        // POST: Submit Adoption Application
+        [HttpPost]
+        public ActionResult Apply(AppDto newApplication)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                
+                string applyUrl = apiBaseUrl + "apply?petId=" + newApplication.PetId;
+                HttpResponseMessage response = client.PostAsJsonAsync(applyUrl, newApplication).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("AppList");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Error submitting application.";
+                    return View("Error");
+                }
+            }
+        }
+
+        // GET: Update Application Form
+        public ActionResult Update(int appId)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string getApplicationUrl = apiBaseUrl + "application/" + appId;
+                HttpResponseMessage response = client.GetAsync(getApplicationUrl).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var application = response.Content.ReadAsAsync<AppDto>().Result;
+                    return View("Update", application);
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Error retrieving application.";
+                    return View("Error");
+                }
+            }
+        }
+
+        // POST: Update Application
+        [HttpPost]
+        public ActionResult Update(int appId, AppDto updatedApplication)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string updateUrl = apiBaseUrl + "update/" + appId;
+                HttpResponseMessage response = client.PutAsJsonAsync(updateUrl, updatedApplication).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("AppList");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Error updating application.";
+                    return View("Error");
+                }
+            }
+        }
+
+        // POST: Delete Application
+        public ActionResult Delete(int appId)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string deleteUrl = apiBaseUrl + "delete/" + appId;
+                HttpResponseMessage response = client.DeleteAsync(deleteUrl).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("AppList");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Error deleting application.";
                     return View("Error");
                 }
             }
